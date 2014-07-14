ba8c3adcdcf4e471617a6178f91bc4bc27f33bef
Sebastian Ramirez <sebastianerp@gmail.com>
2014-07-14 13:37:08 -0400

    ejercicios/admin

--- ejercicios/admin.py
+++ ejercicios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from ejercicios.models import Ejercicio
 
+class EjercicioAdmin(admin.ModelAdmin):
+    list_display = ('anho','actual')
+    
+admin.site.register(Ejercicio, EjercicioAdmin)
 # Register your models here.
