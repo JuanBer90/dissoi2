8e49a2684a1d4a358f984987259a22768049cbcb
Sebastian Ramirez <sebastianerp@gmail.com>
2014-07-14 14:22:25 -0400

    cambios en presupuestos/models.py

--- presupuestos/models.py
+++ presupuestos/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from ejercicios.models import Ejercicio
+from cuentas.models import Cuenta
 
+MESES = (
+    ('1', 'Enero'),
+    ('2', 'Febrero'),
+    ('3', 'Marzo'),
+    ('4', 'Abril'),
+    ('5', 'Mayo'),
+    ('6', 'Junio'),
+    ('7', 'Julio'),
+    ('8', 'Agosto'),
+    ('9', 'Septiembre'),
+    ('10', 'Octubre'),
+    ('11', 'Noviembre'),
+    ('12', 'Diciembre'),
+)
 
+class Presupuesto(models.Model):
+    ejercicio = models.ForeignKey(Ejercicio)
+    mes = models.SmallIntegerField(choices=MESES)
+    cuenta = models.ForeignKey(Cuenta)
+    monto = models.DecimalField(max_digits=22, default=0, decimal_places=2)
+    
+
+    
+    
 # Create your models here.
