ed5cf816f7b69fe4427363892b0f5acde934c326
JuanBer90 <juanber2.0@gmail.com>
2014-07-16 18:57:50 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	hijasdelacaridad/settings.py
    	hijasdelacaridad/settings.pyc
    	hijasdelacaridad/urls.py
    	hijasdelacaridad/urls.pyc

--- presupuestos/views.py
+++ presupuestos/views.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.db.models.aggregates import Count
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 
 # Create your views here.
@@ -17,33 +19,62 @@ from presupuestos.models import Presupuesto, MESES
 def presupuesto(request,tipo):
     if tipo != 'IN' and tipo !='EG':
         tipo='IN'
-
+    cuentas=Cuenta.objects.filter(tipo=tipo).order_by('codigo','numchild')
     ejercicio=Ejercicio.objects.get(actual=True)
-    presupuestos=Presupuesto.objects.filter(ejercicio_id=ejercicio.id).order_by('mes','cuenta__cuenta')
 
-    query="select c.id, c.cuenta, p.mes,sum(p.monto) as monto from presupuestos_presupuesto p"\
+    query="select c.id, c.cuenta, p.mes,sum(p.monto) as monto,c.codigo from presupuestos_presupuesto p"\
     " join ejercicios_ejercicio e on e.id=p.ejercicio_id join cuentas_cuenta c on c.id=p.cuenta_id "\
-    " where e.anho = "+str(ejercicio.anho)+" and c.tipo like '"+tipo+"' group by c.id, c.cuenta,p.mes order by c.cuenta"
-    cuentas=execute_all_query(query)
-    meses= MESES
-    print meses[0][1]
-    cuentas_list=[]
+    " where e.anho = "+str(ejercicio.anho)+" and c.tipo like '"+tipo+"' group by c.id, c.cuenta,c.codigo,p.mes order by c.cuenta"
 
+    presupuestos=execute_all_query(query)
+    presupuestos_list=[]
+
+    cuentas_list=[]
     i=0
-    while i < len(cuentas):
-        aux = [cuentas[i][0], cuentas[i][1]]
+
+    while i < len(presupuestos):
+        aux = [presupuestos[i][0], presupuestos[i][1]]
         for j in range(i, i+12):
-            aux.append(str(cuentas[j][3]))
-        cuentas_list.append(aux)
+            aux.append(str(presupuestos[j][3]))
+        aux.append(presupuestos[i][4])
+        presupuestos_list.append(aux)
         i+=12
+
+    for c in cuentas:
+        existe=esta_en_presupuesto_list(c.id, presupuestos_list)
+        if existe != None:
+            existe.append(False)
+            cuentas_list.append(existe)
+        else:
+            aux = [c.id, c.cuenta]
+            for ii in range(1, 13):
+                aux.append('0.00')
+            aux.append(c.codigo)
+            aux.append(True)
+            cuentas_list.append(aux)
+    for asdf in cuentas_list:
+        print asdf
     if request.method == 'POST':
-        for c in cuentas_list:
+        for c in presupuestos_list:
             for k in range(1,13):
                 monto=request.POST.get('cuenta-'+str(c[0])+'-'+str(k))
                 p=Presupuesto.objects.get(cuenta_id=c[0],mes=k,ejercicio_id=ejercicio.id)
                 p.monto=monto
                 p.save()
-    return render_to_response('balance/presupuesto.html', {'cuentas_list':cuentas_list,'presupuestos':presupuestos,'tipo':tipo, 'MESES':MESES}, context_instance=RequestContext(request))
+        messages.info(request, 'Grabado exitosamente!')
+        save_=request.POST.get('_save','')
+        continue_=request.POST.get('_continue','')
+        if save_ != '':
+            return HttpResponseRedirect('/admin/presupuestos/presupuesto/')
+        if continue_ != '':
+            return HttpResponseRedirect('/presupuesto/'+tipo)
 
 
+    return render_to_response('balance/presupuesto.html', {'presupuestos_list':cuentas_list,'tipo':tipo, 'MESES':MESES}, context_instance=RequestContext(request))
 
+
+def esta_en_presupuesto_list(id,presupuesto_list=[]):
+    for p in presupuesto_list:
+        if p[0] == id:
+            return p
+    return None
